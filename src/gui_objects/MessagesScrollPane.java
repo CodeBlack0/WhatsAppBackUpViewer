4507bf8f2a3a572f7d27a7724316cf9b3363271f
MrSoir <abendtobias1@gmail.com>
2017-05-17 19:16:43 +0200

    scrolling & searching added

--- src/gui_objects/MessagesScrollPane.java
+++ src/gui_objects/MessagesScrollPane.java
@@ -3,22 +3,34 @@ package gui_objects;
 import java.util.ArrayList;
 import java.util.List;
 
+import gui_objects.MessagesPane.ScrBarValueSetter;
+import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import message.Message;
 
 public class MessagesScrollPane extends Pane {
-	List<Message> messages;
-	List<MessageTD> curDipsMsgs = new ArrayList<>();
+	private List<Message> messages;
+	private List<MessageTD> curDipsMsgs = new ArrayList<>();
+		
+	private List<Integer> searchedMsgs = new ArrayList<>();
+	private int curSearchIndx = 0;
 	
-	List<Integer> searchedMsgs = new ArrayList<>();
-	int curSearchIndx = 0;
+	private List<Dim> messageDims = new ArrayList<>();
 	
-	List<Dim> messageDims = new ArrayList<>();
+	private int firstDispMsgIndx = 0;
 	
-	double maxWidth, maxHeight;
+	private Scene scene;
+	private Group scrPneGrp = new Group();
+	
+	private double maxWidth, maxHeight;
+	
+	private double lastMouseDragPosY = -1;
+	
+	private ScrBarValueSetter scrBarValSetter;
 	
 	private class Dim{
 		private double width, height;
@@ -28,21 +40,40 @@ public class MessagesScrollPane extends Pane {
 		}
 	}
 	
-	private int firstDispMsgIndx = 0;
-	private final Scene scene;
-	private Group scrPneGrp = new Group();
-	
-	public MessagesScrollPane(List<Message> messages, final Scene scene){
+	public MessagesScrollPane(List<Message> messages, final Scene scene, final ScrBarValueSetter scrBarValSetter){
 		super();
 		this.messages = messages;
 		this.scene = scene;
+		this.scrBarValSetter = scrBarValSetter;
 		
+		this.setOnScroll(t->{
+			scrBarValSetter.setValue( t.getDeltaY() > 0 ? 1 : -1 );
+		});
+		// key up && down funktioniert noch nicht, da die toolbar staendig den fokus fuer sich beansrpucht...
+//		this.setOnKeyPressed(t->{
+//			if (t.getCode() == KeyCode.UP){
+//				scroll(-1);
+//			}else if (t.getCode() == KeyCode.DOWN){
+//				scroll(1);
+//			}
+//		});
+		this.setOnMousePressed(t->{
+			lastMouseDragPosY = t.getSceneY();
+		});
+		this.setOnMouseDragged(t->{
+			double sceneY = t.getSceneY();
+			double diff = sceneY - lastMouseDragPosY;
+			if (Math.abs(diff) > 10 ){
+				scrBarValSetter.setValueOffset( diff < 0 ? 1 : -1 );
+				lastMouseDragPosY = sceneY;
+			}
+		});
+		this.setOnMouseExited(t->{
+			scene.setCursor(Cursor.DEFAULT);
+		});
 		evalMessageDimensions();
 		
-//		this.setVbarPolicy(ScrollBarPolicy.NEVER);
-
 		this.getChildren().add(scrPneGrp);
-//		this.setContent(scrPneGrp);
 		
 		this.heightProperty().addListener(t->{
 			renderMessages();
@@ -69,24 +100,24 @@ public class MessagesScrollPane extends Pane {
 			}
 		}
 		
-		System.out.printf("evalMessageDimensions: maxWidth: %s	maxHeight: %s%n", maxWidth, maxHeight);
 	}
-	private int getMessageVvalID(double absVvalue){
-		double yOffs = 0;
-		for(int i=0; i < messageDims.size(); i++){
-			if (yOffs >= absVvalue){
-				return i;
-			}
-			yOffs += messageDims.get(i).height;
-		}
-		return -1;
-	}
+//	private int getMessageVvalID(double absVvalue){
+//		double yOffs = 0;
+//		for(int i=0; i < messageDims.size(); i++){
+//			if (yOffs >= absVvalue){
+//				return i;
+//			}
+//			yOffs += messageDims.get(i).height;
+//		}
+//		return -1;
+//	}
 	
-	public void setVValueBD(double vValue){
-		double startHeight = vValue * (maxHeight-this.getHeight());
+	public void setVValueBD(int msgId){
+//		double startHeight = vValue * (maxHeight-this.getHeight());
 		
-		int msgId = getMessageVvalID(startHeight);
-		if (msgId > -1){
+//		int msgId = getMessageVvalID(startHeight);
+		if (messages != null &&
+				msgId > -1 && msgId < messages.size()){
 			firstDispMsgIndx = msgId;
 			renderMessages();
 		}
@@ -101,35 +132,48 @@ public class MessagesScrollPane extends Pane {
 		curDipsMsgs.clear();
 		scrPneGrp.getChildren().clear();
 		
-		for(int i=firstDispMsgIndx; i < messages.size(); i++){
-			MessageTD msgTD = new MessageTD(messages.get(i), cursor->{
-				scene.setCursor(cursor);
-			});
-			
-			curDipsMsgs.add(msgTD);
-			
-			msgTD.setLayoutX(xOffs);
-			msgTD.setLayoutY(yOffs);
-			scrPneGrp.getChildren().add(msgTD);
-			yOffs += msgTD.getBoundsInLocal().getHeight() + insets;
-			
-			if (yOffs > this.getHeight()){
-				break;
+		if (messages != null){
+			for(int i=firstDispMsgIndx; i < messages.size(); i++){
+				MessageTD msgTD = new MessageTD(messages.get(i), cursor->{
+					scene.setCursor(cursor);
+				});
+				
+				curDipsMsgs.add(msgTD);
+				
+				msgTD.setLayoutX(xOffs);
+				msgTD.setLayoutY(yOffs);
+				scrPneGrp.getChildren().add(msgTD);
+				yOffs += msgTD.getBoundsInLocal().getHeight() + insets;
+				
+				if (yOffs > this.getHeight()){
+					break;
+				}
 			}
 		}
 	}
-	
-	public void focusMessage(Message msg){
-		int newMsgIndx = -1;
-		for(int i=0; i < messages.size(); i++){
-			if (msg.equals(messages.get(i))){
-				newMsgIndx = i;
-				break;
+	public void scroll(int msgOffs){
+		focusMessage(firstDispMsgIndx + msgOffs);
+	}
+	public void focusMessage(int msgId){
+		if (messages != null && messages.size() > 0){
+			if (msgId > messages.size()){
+				msgId = messages.size() -1;
+			}
+			if (msgId < 0){
+				msgId = 0;
+			}
+			if (msgId != firstDispMsgIndx){
+				firstDispMsgIndx = msgId;
+				renderMessages();
 			}
 		}
-		if (newMsgIndx > -1 && newMsgIndx != firstDispMsgIndx){
-			firstDispMsgIndx = newMsgIndx;
-			renderMessages();
+	}
+	public void focusMessage(Message msg){
+		if (messages != null && msg != null){
+			int newMsgIndx = messages.indexOf(msg);
+			if (newMsgIndx > -1){
+				focusMessage(newMsgIndx);
+			}
 		}
 	}
 	
@@ -144,12 +188,29 @@ public class MessagesScrollPane extends Pane {
 		curSearchIndx = 0;
 		focusSearch();
 	}
+	
+	public void focusNextSearchResult(){
+		if (searchedMsgs != null && searchedMsgs.size() > 0){
+			if (++curSearchIndx >= searchedMsgs.size()){
+				curSearchIndx = 0;
+			}
+			focusSearch();
+		}
+	}
+	public void focusPrevSearchResult(){
+		if (searchedMsgs != null && searchedMsgs.size() > 0){
+			if (--curSearchIndx < 0){
+				curSearchIndx = searchedMsgs.size() -1;
+			}
+			focusSearch();
+		}
+	}
 	private void focusSearch(){
 		if (curSearchIndx >= 0 && curSearchIndx < searchedMsgs.size()
 				&& searchedMsgs.get(curSearchIndx) > 0
 				&& searchedMsgs.get(curSearchIndx) < messages.size()){
 			focusMessage( messages.get( searchedMsgs.get(curSearchIndx) ) );
-			curSearchIndx += 1;
+			scrBarValSetter.setValue( searchedMsgs.get(curSearchIndx) );
 		}
 	}
 	
@@ -157,7 +218,10 @@ public class MessagesScrollPane extends Pane {
 		messageDims = null;
 		messages = null;
 		curDipsMsgs = null;
+		searchedMsgs = null;
 		closeCurrDispMsgs();
 		scrPneGrp.getChildren().clear();
+		scene = null;
+		scrBarValSetter = null;
 	}
 }
