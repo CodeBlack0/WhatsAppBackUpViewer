796fba9f3c43919c2739eeaab67ca6cb64b274ed
MrSoir <abendtobias1@gmail.com>
2017-05-20 10:47:02 +0200

    regex fixed

--- src/message/TextMessage.java
+++ src/message/TextMessage.java
@@ -8,19 +8,15 @@ package message;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Class for textmessages
- * @author timfi
- */
 public class TextMessage extends Message {
-    public static final Pattern PATTERN = Pattern.compile("(\\d{2}\\.\\d{2}\\.\\d{2}, \\d{1,2}:\\d{2}:\\d{2} (?:AM|PM)): (.+): ((?:.|\n)+)");
+    public static final Pattern PATTERN = Pattern.compile("(\\d{2}\\.\\d{2}\\.\\d{2}, \\d{1,2}:\\d{2}:\\d{2} (?:AM|PM))(:) ([^:]+)(:) ((?:.|\n)+)");
     
     public TextMessage(String line) throws Exception {
         Matcher matcher = PATTERN.matcher(line);   
         if (matcher.find()) {
             this.timestamp = parse_date(matcher.group(1));
-            this.actor = matcher.group(2);
-            this.content = matcher.group(3);
+            this.actor = matcher.group(3);
+            this.content = matcher.group(5);
         } else {
             throw new Exception("Failed to parse textmessage");
         }
